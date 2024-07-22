dc7845777762eb1ba4bfe2cdb7040a5702475330
Sebastian Navales Parra <sebastian.navalesp@gmail.com>
2024-07-22 10:01:04 -0500

    Creación de archivo log de errores de ejecución y requeriments.txt

--- 02_Provisión_Comisiones_Vida/01_Provision_Comisiones_Vida.py
+++ 02_Provisión_Comisiones_Vida/01_Provision_Comisiones_Vida.py
@@ -1,124 +1,146 @@
 import pandas as pd
+import logging
 from scripts import import_transformar as fimptrans
 from scripts import depuracion as fdepuracion
 from scripts import identificacion_ajuste as fajustes
 from scripts import consolidar_export as fconsexport
-from scripts import plantillas_ajustes as fplantajust
-from scripts import plantillas_ajustes_usd as fplantajustusd
 
-#### 1) Inputs para Ejecución del Proceso
+# Arhivo para almacenar errores de ejecución
+class UnderscoreLineFormatter(logging.Formatter):
+    def format(self, record):
+        result = super().format(record)
+        return result + "\n" + "_" * 220 + "\n"
 
-# Funcion para generar la fecha con el ultimo dia del mes de cierre
-mes_cierre,año_cierre,ultimo_dia_del_mes = fimptrans.ultimo_dia_del_mes()
+logging.basicConfig(
+    filename='Errores.txt',
+    level=logging.ERROR,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-#### 2) Definición de Párametros Iniciales
+logger = logging.getLogger()
+for handler in logger.handlers:
+    handler.setFormatter(UnderscoreLineFormatter(handler.formatter._fmt))
 
-# Rango de tolerancia para eliminación de registros con valor muy pequeño
-tolerancia_eliminacon_cero = 0.5
-desde_eliminacion_cero = -tolerancia_eliminacon_cero
-hasta_eliminacion_cero = tolerancia_eliminacon_cero
+try:
+    #### 1) Inputs para Ejecución del Proceso
 
-# Rango de tolerancia para eliminación de pólizas cerradas
-tolerancia_filtros_iniciales = 10
-desde = -tolerancia_filtros_iniciales
-hasta = tolerancia_filtros_iniciales
+    # Funcion para generar la fecha con el ultimo dia del mes de cierre
+    mes_cierre,año_cierre,ultimo_dia_del_mes = fimptrans.ultimo_dia_del_mes()
 
-# Rango de tolerancia para ajustar pólizas en plantilla ajustes financieros
-tolerancia_ajustes_financieros_desde = 10
-tolerancia_ajustes_financieros_hasta = 1000
-desde_positivo = tolerancia_ajustes_financieros_desde
-desde_negativo = -tolerancia_ajustes_financieros_desde
-hasta_positivo = tolerancia_ajustes_financieros_hasta
-hasta_negativo = -tolerancia_ajustes_financieros_hasta
+    #### 2) Definición de Párametros Iniciales
 
-valor_no_ajuste = 6000
-desde_no_ajuste = -valor_no_ajuste
-hasta_no_ajuste = valor_no_ajuste
+    # Rango de tolerancia para eliminación de registros con valor muy pequeño
+    tolerancia_eliminacon_cero = 0.5
+    desde_eliminacion_cero = -tolerancia_eliminacon_cero
+    hasta_eliminacion_cero = tolerancia_eliminacon_cero
 
-# Rango de tolerancia para ajustar pólizas en dólares
-tolerancia_porc_pendientes = 0.1
-desde_porc_pendientes = -tolerancia_porc_pendientes
-hasta_porc_pendientes = tolerancia_porc_pendientes
+    # Rango de tolerancia para eliminación de pólizas cerradas
+    tolerancia_filtros_iniciales = 10
+    desde = -tolerancia_filtros_iniciales
+    hasta = tolerancia_filtros_iniciales
 
-#### 3) Importación tipos de fuente
+    # Rango de tolerancia para ajustar pólizas en plantilla ajustes financieros
+    tolerancia_ajustes_financieros_desde = 10
+    tolerancia_ajustes_financieros_hasta = 1000
+    desde_positivo = tolerancia_ajustes_financieros_desde
+    desde_negativo = -tolerancia_ajustes_financieros_desde
+    hasta_positivo = tolerancia_ajustes_financieros_hasta
+    hasta_negativo = -tolerancia_ajustes_financieros_hasta
 
-# Función para identificar tipos de fuente
-provision,pago,ajuste_provision,ajuste_pago,cdtipo_fuente = fimptrans.tipos_fuente_func()
+    valor_no_ajuste = 6000
+    desde_no_ajuste = -valor_no_ajuste
+    hasta_no_ajuste = valor_no_ajuste
 
-#### 4) Importación archivos necesarios
+    # Rango de tolerancia para ajustar pólizas en dólares
+    tolerancia_porc_pendientes = 0.1
+    desde_porc_pendientes = -tolerancia_porc_pendientes
+    hasta_porc_pendientes = tolerancia_porc_pendientes
 
-# Esquema de datos
-cabecera = {"CDSOCIEDAD": "object",
-            "CDRAMO": "object",
-            "NMPOLIZA": "object",
-            "NMRECIBO": "object",
-            "CDCONCEPTO": "object",
-            "CDCOASEGURO": "object",
-            "CDAGENTE": "object",
-            "CDDELEGACION": "object",
-            "PTPRIMA": "float",
-            "PTCOMISION": "float",
-            "POCOMISION": "float",
-            "CDTIPO_FUENTE": "object",
-            "FEDOCUMENTO": "object",
-            "FEREGISTRO": "object",
-            "FEINI_VIGENCIA": "object",
-            "FEFIN_FIGENCIA": "object",
-            "CDLIBRO": "object",
-            "VACIO": "object"}
+    #### 3) Importación tipos de fuente
 
-cabecera_anterior = {"OBSERVACION": "object",
-                     "AJUSTAR?": "object",
-                     "SIGNO": "float64",
-                     "POSITIVO": "float64",
-                     "NEGATIVO": "float64",
-                     "ABS": "float64",
-                     "CANTIDAD": "float64",
-                     "TIPO": "object",
-                     "TIPO_UNICO": "object",
-                     "DOLARES": "object",
-                     "%PENDIENTE": "float64",
-                     "CDDIR": "object",
-                     "RETIRADOS": "object",
-                     "FEBAJA": "object",
-                     "POST_RETIRO": "object",
-                     "DIAS_RETIRO": "float64",
-                     "FYD": "object",
-                     "COMENTARIO": "object"}
+    # Función para identificar tipos de fuente
+    provision,pago,ajuste_provision,ajuste_pago,cdtipo_fuente = fimptrans.tipos_fuente_func()
 
-cabecera_anterior_completo = cabecera | cabecera_anterior
-del cabecera_anterior_completo["VACIO"]
+    #### 4) Importación archivos necesarios
 
-# Función para importar archivo principal comisiones
-vida = fimptrans.importar_archivo_principal(mes_cierre,año_cierre,cabecera_anterior_completo,cabecera,cabecera_anterior,desde_eliminacion_cero, hasta_eliminacion_cero)
+    # Esquema de datos
+    cabecera = {"CDSOCIEDAD": "object",
+                "CDRAMO": "object",
+                "NMPOLIZA": "object",
+                "NMRECIBO": "object",
+                "CDCONCEPTO": "object",
+                "CDCOASEGURO": "object",
+                "CDAGENTE": "object",
+                "CDDELEGACION": "object",
+                "PTPRIMA": "float",
+                "PTCOMISION": "float",
+                "POCOMISION": "float",
+                "CDTIPO_FUENTE": "object",
+                "FEDOCUMENTO": "object",
+                "FEREGISTRO": "object",
+                "FEINI_VIGENCIA": "object",
+                "FEFIN_FIGENCIA": "object",
+                "CDLIBRO": "object",
+                "VACIO": "object"}
 
-# Función para importar insumos varios
-dolares,retirados,codigos_directos,asesores_formacion = fimptrans.importar_insumos_varios()
+    cabecera_anterior = {"OBSERVACION": "object",
+                        "AJUSTAR?": "object",
+                        "SIGNO": "float64",
+                        "POSITIVO": "float64",
+                        "NEGATIVO": "float64",
+                        "ABS": "float64",
+                        "CANTIDAD": "float64",
+                        "TIPO": "object",
+                        "TIPO_UNICO": "object",
+                        "DOLARES": "object",
+                        "%PENDIENTE": "float64",
+                        "CDDIR": "object",
+                        "RETIRADOS": "object",
+                        "FEBAJA": "object",
+                        "POST_RETIRO": "object",
+                        "DIAS_RETIRO": "float64",
+                        "FYD": "object",
+                        "COMENTARIO": "object"}
 
-#### 5) Validación CDTIPO_FUENTE
+    cabecera_anterior_completo = cabecera | cabecera_anterior
+    del cabecera_anterior_completo["VACIO"]
 
-# Función para válidar el tipo de fuente
-fimptrans.validar_tipo_fuente(vida,cdtipo_fuente)
+    # Función para importar archivo principal comisiones
+    vida = fimptrans.importar_archivo_principal(mes_cierre,año_cierre,cabecera_anterior_completo,cabecera,cabecera_anterior,desde_eliminacion_cero, hasta_eliminacion_cero)
 
-#### 6) Transformaciones iniciales
+    # Función para importar insumos varios
+    dolares,retirados,codigos_directos,asesores_formacion = fimptrans.importar_insumos_varios()
 
-# Función para transformaciones iniciales del dataframe principal
-vida = fimptrans.transformaciones_iniciales(vida,ajuste_pago,pago)
+    #### 5) Validación CDTIPO_FUENTE
 
-#### 7) Depuración de Tablas
+    # Función para válidar el tipo de fuente
+    fimptrans.validar_tipo_fuente(vida,cdtipo_fuente)
 
-# Función para depuración de tablas
-vida = fdepuracion.depuracion(vida,desde,hasta,provision)
+    #### 6) Transformaciones iniciales
 
-#### 8) Identificación Ajustes Financieros
+    # Función para transformaciones iniciales del dataframe principal
+    vida = fimptrans.transformaciones_iniciales(vida,ajuste_pago,pago)
 
-# Función para identificar registros que se deben ajustar
-vida_ajustados1,vida_ajustados2,vida_ajustados3,vida_ajustados4,vida_ajustados5,vida_revision1,vida_revision2,vida_revision3,vida_revision4,vida_revision5 = fajustes.identificacion_ajustes(vida,hasta,desde,hasta_negativo,desde_negativo,hasta_positivo,desde_positivo,provision,desde_no_ajuste,hasta_no_ajuste)
+    #### 7) Depuración de Tablas
 
-#### 9) Consolidar Tablas y Exportar
+    # Función para depuración de tablas
+    vida = fdepuracion.depuracion(vida,desde,hasta,provision)
 
-# Generación de tabla generales nuevamente con registros ajustados y registros a revisar
-vida = pd.concat([vida_revision5, vida_ajustados1, vida_ajustados2, vida_ajustados3, vida_ajustados4, vida_ajustados5])
+    #### 8) Identificación Ajustes Financieros
 
-# Función para consolidar tablas y exportar
-vida, vida_ajustados6 = fconsexport.consolidar_exportar(vida,provision,pago,dolares,desde_porc_pendientes,hasta_porc_pendientes,codigos_directos,retirados,asesores_formacion,mes_cierre,año_cierre)
+    # Función para identificar registros que se deben ajustar
+    vida_ajustados1,vida_ajustados2,vida_ajustados3,vida_ajustados4,vida_ajustados5,vida_revision1,vida_revision2,vida_revision3,vida_revision4,vida_revision5 = fajustes.identificacion_ajustes(vida,hasta,desde,hasta_negativo,desde_negativo,hasta_positivo,desde_positivo,provision,desde_no_ajuste,hasta_no_ajuste)
+
+    #### 9) Consolidar Tablas y Exportar
+
+    # Generación de tabla generales nuevamente con registros ajustados y registros a revisar
+    vida = pd.concat([vida_revision5, vida_ajustados1, vida_ajustados2, vida_ajustados3, vida_ajustados4, vida_ajustados5])
+
+    # Función para consolidar tablas y exportar
+    vida, vida_ajustados6 = fconsexport.consolidar_exportar(vida,provision,pago,dolares,desde_porc_pendientes,hasta_porc_pendientes,codigos_directos,retirados,asesores_formacion,mes_cierre,año_cierre)
+
+except Exception as e:
+    print("Ocurrió un error. Revisar el archivo Errores.txt")
+    input("Presione Enter para salir...")
+    logging.error("Ocurrió un error:", exc_info=True)
--- 02_Provisión_Comisiones_Vida/02_Plantillas_Ajustes_Financieros.py
+++ 02_Provisión_Comisiones_Vida/02_Plantillas_Ajustes_Financieros.py
@@ -1,61 +1,85 @@
+import logging
 from scripts import import_transformar as fimptrans
 from scripts import plantillas_ajustes as fplantajust
 from scripts import plantillas_ajustes_usd as fplantajustusd
 
-#### 1) Inputs para Ejecución del Proceso
+# Arhivo para almacenar errores de ejecución
+class UnderscoreLineFormatter(logging.Formatter):
+    def format(self, record):
+        result = super().format(record)
+        return result + "\n" + "_" * 220 + "\n"
 
-# Funcion para generar la fecha con el ultimo dia del mes de cierre
-mes_cierre,año_cierre,ultimo_dia_del_mes = fimptrans.ultimo_dia_del_mes()
+logging.basicConfig(
+    filename='Errores.txt',
+    level=logging.ERROR,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-#### 2) Importación archivos necesarios
+logger = logging.getLogger()
+for handler in logger.handlers:
+    handler.setFormatter(UnderscoreLineFormatter(handler.formatter._fmt))
 
-# Esquema de datos
-cabecera_anterior_completo = {"CDSOCIEDAD": "object",
-            "CDRAMO": "object",
-            "NMPOLIZA": "object",
-            "NMRECIBO": "object",
-            "CDCONCEPTO": "object",
-            "CDCOASEGURO": "object",
-            "CDAGENTE": "object",
-            "CDDELEGACION": "object",
-            "PTPRIMA": "float",
-            "PTCOMISION": "float",
-            "POCOMISION": "float",
-            "CDTIPO_FUENTE": "object",
-            "FEDOCUMENTO": "object",
-            "FEREGISTRO": "object",
-            "FEINI_VIGENCIA": "object",
-            "FEFIN_FIGENCIA": "object",
-            "CDLIBRO": "object",
-            "OBSERVACION": "object",
-            "AJUSTAR?": "object",
-            "SIGNO": "float64",
-            "POSITIVO": "float64",
-            "NEGATIVO": "float64",
-            "ABS": "float64",
-            "CANTIDAD": "float64",
-            "TIPO": "object",
-            "TIPO_UNICO": "object",
-            "DOLARES": "object",
-            "%PENDIENTE": "float64",
-            "CDDIR": "object",
-            "RETIRADOS": "object",
-            "FEBAJA": "object",
-            "POST_RETIRO": "object",
-            "DIAS_RETIRO": "float64",
-            "FECHA_INGRESO" : "object",
-            "FYD": "object",
-            "COMENTARIO": "object"}
+try:
+    #### 1) Inputs para Ejecución del Proceso
 
-# Función para leer archivo depurado para Ajustes Financieros
-vida_ajustes = fimptrans.lectura_ajustes(mes_cierre,año_cierre,cabecera_anterior_completo)
+    # Funcion para generar la fecha con el ultimo dia del mes de cierre
+    mes_cierre,año_cierre,ultimo_dia_del_mes = fimptrans.ultimo_dia_del_mes()
 
-#### 3) Generar Plantillas de Ajustes Financieros
+    #### 2) Importación archivos necesarios
 
-# Función para generar plantillas de ajustes financieros
-fplantajust.plantillas_ajustes(vida_ajustes,ultimo_dia_del_mes,mes_cierre,año_cierre)
+    # Esquema de datos
+    cabecera_anterior_completo = {"CDSOCIEDAD": "object",
+                "CDRAMO": "object",
+                "NMPOLIZA": "object",
+                "NMRECIBO": "object",
+                "CDCONCEPTO": "object",
+                "CDCOASEGURO": "object",
+                "CDAGENTE": "object",
+                "CDDELEGACION": "object",
+                "PTPRIMA": "float",
+                "PTCOMISION": "float",
+                "POCOMISION": "float",
+                "CDTIPO_FUENTE": "object",
+                "FEDOCUMENTO": "object",
+                "FEREGISTRO": "object",
+                "FEINI_VIGENCIA": "object",
+                "FEFIN_FIGENCIA": "object",
+                "CDLIBRO": "object",
+                "OBSERVACION": "object",
+                "AJUSTAR?": "object",
+                "SIGNO": "float64",
+                "POSITIVO": "float64",
+                "NEGATIVO": "float64",
+                "ABS": "float64",
+                "CANTIDAD": "float64",
+                "TIPO": "object",
+                "TIPO_UNICO": "object",
+                "DOLARES": "object",
+                "%PENDIENTE": "float64",
+                "CDDIR": "object",
+                "RETIRADOS": "object",
+                "FEBAJA": "object",
+                "POST_RETIRO": "object",
+                "DIAS_RETIRO": "float64",
+                "FECHA_INGRESO" : "object",
+                "FYD": "object",
+                "COMENTARIO": "object"}
 
-#### 4) Generar Plantillas de Ajustes Financieros USD
+    # Función para leer archivo depurado para Ajustes Financieros
+    vida_ajustes = fimptrans.lectura_ajustes(mes_cierre,año_cierre,cabecera_anterior_completo)
 
-# Función para generar plantillas de ajustes financieros en USD
-fplantajustusd.plantillas_ajustes_usd(vida_ajustes,ultimo_dia_del_mes,mes_cierre,año_cierre)
+    #### 3) Generar Plantillas de Ajustes Financieros
+
+    # Función para generar plantillas de ajustes financieros
+    fplantajust.plantillas_ajustes(vida_ajustes,ultimo_dia_del_mes,mes_cierre,año_cierre)
+
+    #### 4) Generar Plantillas de Ajustes Financieros USD
+
+    # Función para generar plantillas de ajustes financieros en USD
+    fplantajustusd.plantillas_ajustes_usd(vida_ajustes,ultimo_dia_del_mes,mes_cierre,año_cierre)
+
+except Exception as e:
+    print("Ocurrió un error. Revisar el archivo Errores.txt")
+    input("Presione Enter para salir...")
+    logging.error("Ocurrió un error:", exc_info=True)
